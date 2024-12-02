e1a94894b6be60e3d2ba272400b99bb87e49dd86
joseomarrb <omarrosadoj@gmail.com>
2024-12-02 18:25:13 -0400

    Ejercicios Objetos, Destructuring y Propagation Completados

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -4,21 +4,51 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=15675
 */
 
 // 1. Crea un objeto con 3 propiedades
+let myObjeto = {
+    nombres: 'José Omar',
+    apellidos: 'Rosado Bueno',
+    edad: 19,
+    datos () {
+        return `${this.nombres} ${this.apellidos}`
+    }
+};
 
 // 2. Accede y muestra su valor
+console.log(myObjeto.nombres);
+console.log(myObjeto.apellidos);
+console.log(myObjeto.edad);
 
 // 3. Agrega una nueva propiedad
+myObjeto.actividades = 'Programación, Deportes, Familia'
 
 // 4. Elimina una de las 3 primeras propiedades
+delete myObjeto.edad
 
 // 5. Agrega una función e invócala
+console.log(myObjeto.datos());
 
 // 6. Itera las propiedades del objeto
+for (const prop in myObjeto) {
+    console.log(prop, myObjeto[prop]);
+}
 
 // 7. Crea un objeto anidado
+const objetoAnidado = {
+    nombres : 'Liz Angeline',
+    apellidos: 'Santiago Checo',
+    edad: 18,
+    familia : {
+        madre : 'Yinaury',
+        padre : 'Alexis'
+    }
+}
 
 // 8. Accede y muestra el valor de las propiedades anidadas
+console.log(objetoAnidado.familia.madre);
+console.log(objetoAnidado.familia.padre);
 
 // 9. Comprueba si los dos objetos creados son iguales
+console.log(objetoAnidado === myObjeto);
 
 // 10. Comprueba si dos propiedades diferentes son iguales
+console.log(objetoAnidado.nombres === myObjeto.nombres);
--- Basic/21-destructuring-spreading-exercises.js
+++ Basic/21-destructuring-spreading-exercises.js
@@ -4,22 +4,58 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=16802
 */
 
 // 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const [ uno, dos ] = myArray;
 
 // 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
+const [ , , tres = 0, cuatro = 0, cinco = 0] = myArray
 
 // 3. Usa desestructuración para extraer dos propiedades de un objeto
+let myObjeto = {
+    nombres: 'José Omar',
+    apellidos: 'Rosado Bueno',
+    edad: 19,
+};
+
+const {nombres, apellidos} = myObjeto;
 
 // 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
 //    a nuevas variables con nombres diferentes
+let car = {
+    marca: 'Mustang',
+    year: 1998,
+};
+const { marca : marcas, year : years} = car;
 
 // 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+const omarObject = {
+    nombres: 'José Omar',
+    apellidos: 'Rosado Bueno',
+    edad: 19,
+    familia : {
+        madre: 'Apolonia Bueno',
+        padre: 'José Osvaldo Rosado',
+        hermanoMayor: 'José Oneal Rosado',
+        hermanoMenor: 'Oscar José Rosado',
+    }
+}
+
+const { familia : {madre, padre, hermanoMayor, hermanoMenor} } = omarObject
 
 // 6. Usa propagación para combinar dos arrays en uno nuevo
+const segundoArray = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+const combinacionArray = [...myArray, ...segundoArray];
 
 // 7. Usa propagación para crear una copia de un array
+const nuevoArray = [...myArray];
 
 // 8. Usa propagación para combinar dos objetos en uno nuevo
+const nuevoObjeto = {...car, ...omarObject}
 
 // 9. Usa propagación para crear una copia de un objeto
+const copiarCar = {...car}
 
 // 10. Combina desestructuración y propagación
+const [ , , trece, ...resto ] = segundoArray;
+const { marca, year, ...otras } = car;
