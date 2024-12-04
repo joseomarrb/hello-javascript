73a63224ae5b77ad1f3d8c38d6691979cb49f9a9
joseomarrb <omarrosadoj@gmail.com>
2024-12-04 18:19:47 -0400

    Ejercicios de Clases Completados

--- Basic/23-classes-exercises.js
+++ Basic/23-classes-exercises.js
@@ -4,18 +4,71 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=18630
 */
 
 // 1. Crea una clase que reciba dos propiedades
+class firstClass {
+    constructor(name, lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    props() {
+        // 2
+        return `${this.name} ${this.lastName} ♥`
+    }
+
+    static datos(a, b) {
+        // 4
+        return a + b
+    }
+};
 
 // 2. Añade un método a la clase que utilice las propiedades
 
 // 3. Muestra los valores de las propiedades e invoca a la función
+const instanciaFirstClass = new firstClass('José Omar', 'Rosado Bueno');
+console.log(instanciaFirstClass.props());
 
 // 4. Añade un método estático a la primera clase
 
 // 5. Haz uso del método estático
+console.log(firstClass.datos(10, 20));
 
 // 6. Crea una clase que haga uso de herencia
+class herencia extends firstClass {
+    props() {
+        // 10
+        return `Metodo Modificado ♥`
+    }
+
+    output() {
+        return `Esta es una clase herenciada`
+    }
+}
 
 // 7. Crea una clase que haga uso de getters y setters
+class getAndSet {
+    // 8
+    #addrees
+    
+    constructor(name, lastName, city, address) {
+        this.name = name;
+        this.lastName = lastName;
+        this.city = city;
+        this.#addrees = address;
+    }
+
+    get data() {
+        // 9
+        return `${this.name} ${this.lastName}, Ciudad ${this.city}`
+    }
+
+    set modifyAddress(address) {
+        this.#addrees = address;
+    }
+}
+
+const datas = new getAndSet('Liz', 'Santiago', 'Santo Domingo', 'Mega Centro');
+console.log(datas.data);
+datas.modifyAddress = 'San Isidro';
 
 // 8. Modifica la clase con getters y setters para que use propiedades privadas
 
